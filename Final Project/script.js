95c423e86fe67722975235bf4a3201ec3411bebe
amandawilliams11 <53537596+amandawilliams11@users.noreply.github.com>
2022-05-05 08:16:17 -0400

    update

---
+++ Final Project/script.js
@@ -0,0 +1,11 @@
+
+// the function is triggeres when the button with the onclick action is selected. sets the with from 0 (which means its hidden) to its full width of visibility
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+
+// this function does the opposite when the button is triggered
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
